c23a40a8ec9b71a9df587a37c250c74d417a3a56
songhaibei <songhaibei7305@163.com>
2022-04-03 10:58:26 +0800

    配置路由和链接跳转

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BasicComponent } from './button/basic/basic.component';
+import { ListComponent} from './data-display/list/list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'button/basic', component: BasicComponent }
+  { path: 'button/basic', component: BasicComponent },
+  { path: 'display-list/list', component: ListComponent }
 ];
 
 @NgModule({
