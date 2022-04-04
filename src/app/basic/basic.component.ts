0293194c951b72425559440fc88ab7ba6e66ba2f
songhaibei <songhaibei7305@163.com>
2022-04-05 02:47:13 +0800

    上js代码

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var layui: any;
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
@@ -10,6 +11,29 @@ export class BasicComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    layui.use(['dropdown', 'util'], function () {
+      var dropdown = layui.dropdown
+        , util = layui.util
+        , $ = layui.jquery;
+
+      //菜单点击事件
+      dropdown.on('click(demo1)', function (options) {
+        var thisElem = $(this);
+        console.log(thisElem, options);
+      });
+
+
+      util.event('lay-active', {
+        normal: function () {
+          $('#demo-box').children().addClass('layui-col-md3').removeClass('layui-col-md4');
+          $('#demo-box').find('.layui-menu').removeClass('layui-menu-lg');
+        }
+        , lg: function () {
+          $('#demo-box').children().addClass('layui-col-md4').removeClass('layui-col-md3')
+          $('#demo-box').find('.layui-menu').addClass('layui-menu-lg');
+        }
+      });
+    });
   }
 
 }
