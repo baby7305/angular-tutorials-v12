cf496c0fda7e2710ead064a3215d5da36d6c677a
songhaibei <songhaibei7305@163.com>
2022-04-05 19:07:39 +0800

    上样式

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-basic',
@@ -10,6 +11,24 @@ export class BasicComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const ec = echarts as any;
+    const lineChart = ec.init(document.getElementById('lineChart'));
+
+    const lineChartOption = {
+
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+      }]
+    }
+    lineChart.setOption(lineChartOption);
   }
 
 }
