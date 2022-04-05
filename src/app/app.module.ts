9b7715a95a5d4dd9910a6bd3d669816dd1a02e6b
songhaibei <songhaibei7305@163.com>
2022-04-05 21:45:31 +0800

    配置引用module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EasyUIModule } from 'ng-easyui'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    EasyUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
