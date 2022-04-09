51ba312e282e71ff90a7703567c38e28f660454b
songhaibei <songhaibei7305@163.com>
2022-04-09 11:26:10 +0800

    上样式

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-basic',
@@ -10,6 +11,14 @@ export class BasicComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(function () {
+      $("#myButton1").click(function () {		//响应单击按钮“显示原始文本”
+        $("#p2")[0].className = "font-weight-normal";
+      });
+      $("#myButton2").click(function () {		//响应单击按钮“显示加细文本”
+        $("#p2")[0].className = "font-weight-light";
+      });
+    });
   }
 
 }
