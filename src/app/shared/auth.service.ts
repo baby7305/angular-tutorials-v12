77793e5dac3c1b965c0ef4eb0c1eb896e5113c22
songhaibei <songhaibei7305@163.com>
2022-04-10 13:42:34 +0800

    配置service方法

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private fireauth: AngularFireAuth, private router: Router) { }
+
+  // login method
+  login(email: string, password: string) {
+    this.fireauth.signInWithEmailAndPassword(email, password).then(() => {
+      localStorage.setItem('token', 'true')
+      this.router.navigate(['dashboard']);
+    }, err => {
+      alert(err);
+      this.router.navigate(['/login']);
+    })
+  }
+
+  // register method
+  register(email: string, password: string) {
+    this.fireauth.createUserWithEmailAndPassword(email, password).then(() => {
+      alert('registration Sucessful');
+      this.router.navigate(['/login']);
+    }, err => {
+      alert(err.message);
+      this.router.navigate(['/login']);
+    })
+  }
+
+  // sign out
+  logout() {
+    this.fireauth.signOut().then(() => {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    }, err => {
+      alert(err.message);
+      this.router.navigate(['/login']);
+    })
+  }
 }
