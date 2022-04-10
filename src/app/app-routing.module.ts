2b9c936f6c8c22bc627fdd7f52af865b695ff971
songhaibei <songhaibei7305@163.com>
2022-04-10 13:42:24 +0800

    配置路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BasicComponent } from './basic/basic.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { RegisterComponent } from './component/register/register.component';
 
 const routes: Routes = [
-  { path: 'basic', component: BasicComponent }
+  { path: 'basic', component: BasicComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
