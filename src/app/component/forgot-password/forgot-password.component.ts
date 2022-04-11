b718391e14872070b32803ed44f6cbb2ddaf171f
songhaibei <songhaibei7305@163.com>
2022-04-12 02:05:09 +0800

    配置forgot-password组件

--- src/app/component/forgot-password/forgot-password.component.ts
+++ src/app/component/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  email: string = '';
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  forgotPassword() {
+    this.auth.forgotPassword(this.email);
+    this.email = '';
+
   }
 
 }
